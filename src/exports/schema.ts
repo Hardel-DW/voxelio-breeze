7b4dc4e3cc493b9d7dd28077e925da0997fa81e1
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-02-08 04:25:41 +0100

    Added import for schema

---
+++ src/exports/schema.ts
@@ -0,0 +1,4 @@
+export * from "@/schema/enchantment/Enchantment";
+export * from "@/schema/enchantment/EnchantmentCost";
+export * from "@/schema/text/TextComponentType";
+export * from "@/schema/tag/TagType";
